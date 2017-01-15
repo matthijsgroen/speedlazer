51cc890cf17802878fe4c7d7b2ae507e09f409b5
Matthijs Groen <matthijs.groen@gmail.com>
2017-01-15 21:42:49 +0100

    Start of new engine

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path    = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    game: "./app/game.js"
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: '[name].js',
+    chunkFilename: '[name]-[hash].js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({minimize: true})
+  ]
+};
