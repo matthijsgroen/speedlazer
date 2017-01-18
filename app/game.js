69e6137d7ae18950ce1df593abc4f837f81fa3fc
Matthijs Groen <matthijs.groen@gmail.com>
2017-01-18 13:28:20 +0100

    Make it a bit more demo-able

--- app/game.js
+++ app/game.js
@@ -5,27 +5,31 @@ Crafty.init(800,600, document.getElementById('game'));
 Crafty.background('#000');
 
 Crafty.e('2D, WebGL, Color, Fourway').attr({
-  x: 0,
-  y: 0,
+  x: 200,
+  y: 200,
   w: 20,
-  h: 20
-}).color('#F00').fourway(100).bind('MotionChange', function(property) {
+  h: 20,
+  z: 5
+}).color('#F00');
+
+Crafty.e('2D, Fourway').fourway(100).bind('MotionChange', function(property) {
   let newValue = {};
   newValue[property.key] = this[property.key];
   Crafty.s('CameraSystem').camera.attr(newValue);
 })
 
 Crafty.e('2D, WebGL, Color, CameraRelativeMotion').attr({
-  x: 400,
-  y: 400,
-  w: 20,
-  h: 20
+  x: 700,
+  y: 100,
+  w: 30,
+  h: 400,
+  z: 10
 }).color('#00F')
 
 Crafty.e('2D, WebGL, Color, CameraRelativeMotion').attr({
-  x: 300,
-  y: 200,
+  x: 500,
+  y: 150,
   w: 20,
-  h: 20
+  h: 300
 }).color('#0F0').cameraRelativeMotion({ xResponse: 0.5, yResponse: 0.5 });
 
