eb35ad46d44423688124a81f0cdb99ec661739dc
Matthijs Groen <matthijs.groen@gmail.com>
2017-01-22 18:25:19 +0100

    Scale game again

--- app/game.js
+++ app/game.js
@@ -4,7 +4,7 @@ import 'app/scenes/loading'
 import 'app/scenes/intro'
 import 'app/scenes/gameplay'
 
-Crafty.init(800,600, document.getElementById('game'))
+Crafty.init(1024,576, document.getElementById('game'))
 Crafty.background('#000')
 
 Crafty.createLayer('UILayerDOM', 'DOM', {
@@ -15,6 +15,45 @@ Crafty.createLayer('UILayerDOM', 'DOM', {
 })
 
 Crafty.scene('Loading')
+
+function scaleGame() {
+  const gameElement = document.getElementById('game')
+  let stageHeight = gameElement.clientHeight,
+    stageWidth = gameElement.clientWidth,
+    viewportHeight = window.innerHeight - 50,
+    viewportWidth = window.innerWidth
+
+  let ratioY = viewportHeight / stageHeight;
+  let ratioX = viewportWidth / stageWidth;
+  let ratio = Math.min(ratioY, ratioX);
+
+  gameElement.style.transform = "scale(" + ratio + ")"
+
+  //$('footer').css({ top: (576 * ratio) });
+}
+
+window.addEventListener("resize", () => {
+  scaleGame();
+})
+
+// Handle the fullscreen button
+//$(document).on('click', '#cr-stage', function () {
+  //if (screenfull.enabled) {
+    //screenfull.request($('#theater')[0]);
+    //$('body').addClass('fullscreen');
+    //scaleGame();
+    //document.addEventListener(screenfull.raw.fullscreenchange, function () {
+      //if (!screenfull.isFullscreen) {
+        //// exit fullscreen code here
+        //$('body').removeClass('fullscreen');
+        //scaleGame();
+      //}
+    //});
+  //}
+//});
+
+setTimeout(function() { scaleGame(); }, 0);
+
 
 // Control zooming
 
