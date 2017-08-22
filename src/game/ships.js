f21eab48bedc5d2be653f56d254e282369af3d23
Matthijs Groen <matthijs.groen@gmail.com>
2017-08-22 21:26:15 +0200

    Shoot

--- src/game/ships.js
+++ src/game/ships.js
@@ -6,13 +6,64 @@ const playerShips = [];
 const ControllableShip = "ControllableShip";
 
 Crafty.c(ControllableShip, {
-  updateState: function({ controls }) {
+  init: function() {
+    this.bind("UpdatedProps", this.controlShip);
+  },
+  controlShip: function({ controls }) {
     const xAxis = controls.right - controls.left;
     const yAxis = controls.down - controls.up;
     this.attr({
       vx: xAxis * 200,
       vy: yAxis * 200
     });
+  }
+});
+
+const RemoveOutOfScreen = "RemoveOutOfScreen";
+
+Crafty.c(RemoveOutOfScreen, {
+  init: function() {
+    this.bind("EnterFrame", this.checkBoundaries);
+  },
+  removed: function() {
+    this.unbind("EnterFrame", this.checkBoundaries);
+  },
+  checkBoundaries: function() {
+    const inBounds = this.x < Crafty.viewport.x + Crafty.viewport.width;
+
+    if (!inBounds) {
+      this.destroy();
+    }
+  }
+});
+
+const WeaponSystems = "WeaponSystems";
+
+Crafty.c(WeaponSystems, {
+  init: function() {
+    this.bind("UpdatedProps", this.fireWeapons);
+  },
+  fireWeapons: function({ controls }) {
+    if (controls.fire && !this.fireWeapon) {
+      this.fireWeapon = true;
+      this.fireInterval = setInterval(() => {
+        this.shoot();
+      }, 100);
+    } else if (!controls.fire && this.fireWeapon) {
+      this.fireWeapon = false;
+      clearInterval(this.fireInterval);
+    }
+  },
+  shoot: function() {
+    Crafty.e("2D, WebGL, Color, Motion, RemoveOutOfScreen")
+      .attr({
+        x: this.x + this.w,
+        y: this.y + this.h / 2,
+        vx: 600,
+        w: 20,
+        h: 5
+      })
+      .color("#FFDDDD");
   }
 });
 
@@ -27,7 +78,15 @@ const updateShips = props => {
     const shipProps = { ship, controls, player };
     if (!shipEntity) {
       shipEntity = Crafty.e(
-        ["2D", "WebGL", "Color", ControllableShip, "Motion", Props].join(",")
+        [
+          "2D",
+          "WebGL",
+          "Color",
+          ControllableShip,
+          "Motion",
+          Props,
+          WeaponSystems
+        ].join(",")
       )
         .attr({
           x: 160,
@@ -38,7 +97,7 @@ const updateShips = props => {
         .color(player.color);
       playerShips.push(shipEntity);
     }
-    shipEntity.props(shipProps);
+    shipEntity.trigger("UpdatedProps", shipProps);
   }
   if (playerShips.length > shipsArray.length) {
     for (let c = shipsArray.length; c < playerShips.length; c++) {
